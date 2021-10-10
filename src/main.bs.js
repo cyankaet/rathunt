8a38895ffee7b9095732b7dde49d59818c20af00
Kate Meuse <katemeuse@gmail.com>
2021-10-10 14:14:02 -0400

    remove dune references, begin chess tutorial

--- src/main.bs.js
+++ src/main.bs.js
@@ -1,84 +1,75 @@
 
 
+import * as Curry from "bs-platform/lib/es6/curry.js";
+import * as Printf from "bs-platform/lib/es6/printf.js";
 import * as Tea_app from "bucklescript-tea/src-ocaml/tea_app.bs.js";
 import * as Tea_html from "bucklescript-tea/src-ocaml/tea_html.bs.js";
 
-function set(param_0) {
-  return /* Set */{
-          _0: param_0
+var model = {
+  moves: 1,
+  turn: /* White */1
+};
+
+function update(model, param) {
+  var match = model.turn;
+  var turn = match ? /* Black */0 : /* White */1;
+  var moves = model.moves + 1 | 0;
+  return {
+          moves: moves,
+          turn: turn
         };
 }
 
-function init(param) {
-  return 4;
-}
-
-function update(model, v) {
-  if (typeof v !== "number") {
-    return v._0;
-  }
-  switch (v) {
-    case /* Increment */0 :
-        return model + 1 | 0;
-    case /* Decrement */1 :
-        return model - 1 | 0;
-    case /* Reset */2 :
-        return 0;
-    
-  }
-}
-
-function view_button(title, msg) {
-  return Tea_html.button(undefined, undefined, {
-              hd: Tea_html.onClick(msg),
-              tl: /* [] */0
-            }, {
-              hd: Tea_html.text(title),
-              tl: /* [] */0
-            });
-}
-
 function view(model) {
+  var match = model.turn;
   return Tea_html.div(undefined, undefined, /* [] */0, {
-              hd: Tea_html.span(undefined, undefined, {
-                    hd: Tea_html.style("text-weight", "bold"),
-                    tl: /* [] */0
-                  }, {
-                    hd: Tea_html.text(String(model)),
+              hd: Tea_html.p(undefined, undefined, /* [] */0, {
+                    hd: Tea_html.text(Curry._2(Printf.sprintf(/* Format */{
+                                  _0: {
+                                    TAG: /* String_literal */11,
+                                    _0: "Move ",
+                                    _1: {
+                                      TAG: /* Int */4,
+                                      _0: /* Int_d */0,
+                                      _1: /* No_padding */0,
+                                      _2: /* No_precision */0,
+                                      _3: {
+                                        TAG: /* String_literal */11,
+                                        _0: ".  It is ",
+                                        _1: {
+                                          TAG: /* String */2,
+                                          _0: /* No_padding */0,
+                                          _1: {
+                                            TAG: /* String_literal */11,
+                                            _0: "'s move.",
+                                            _1: /* End_of_format */0
+                                          }
+                                        }
+                                      }
+                                    }
+                                  },
+                                  _1: "Move %d.  It is %s's move."
+                                }), model.moves, match ? "White" : "Black")),
                     tl: /* [] */0
                   }),
               tl: {
-                hd: Tea_html.br(/* [] */0),
-                tl: {
-                  hd: view_button("Increment", /* Increment */0),
-                  tl: {
-                    hd: Tea_html.br(/* [] */0),
-                    tl: {
-                      hd: view_button("Decrement", /* Decrement */1),
-                      tl: {
-                        hd: Tea_html.br(/* [] */0),
-                        tl: {
-                          hd: view_button("Set to 42", /* Set */{
-                                _0: 42
-                              }),
-                          tl: {
-                            hd: Tea_html.br(/* [] */0),
-                            tl: {
-                              hd: model !== 0 ? view_button("Reset", /* Reset */2) : Tea_html.noNode,
-                              tl: /* [] */0
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
+                hd: Tea_html.p(undefined, undefined, /* [] */0, {
+                      hd: Tea_html.button(undefined, undefined, {
+                            hd: Tea_html.onClick(/* Move */0),
+                            tl: /* [] */0
+                          }, {
+                            hd: Tea_html.text("Make a move!"),
+                            tl: /* [] */0
+                          }),
+                      tl: /* [] */0
+                    }),
+                tl: /* [] */0
               }
             });
 }
 
 var partial_arg = {
-  model: 4,
+  model: model,
   update: update,
   view: view
 };
@@ -87,20 +78,9 @@ function main(param, param$1) {
   return Tea_app.beginnerProgram(partial_arg, param, param$1);
 }
 
-var increment = /* Increment */0;
-
-var decrement = /* Decrement */1;
-
-var reset = /* Reset */2;
-
 export {
-  increment ,
-  decrement ,
-  reset ,
-  set ,
-  init ,
+  model ,
   update ,
-  view_button ,
   view ,
   main ,
   
