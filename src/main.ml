22cdbeea46625b548ea99caff14199cf45de9ff3
Kate Meuse <katemeuse@gmail.com>
2021-10-10 15:31:20 -0400

    finally format ocaml files

--- src/main.ml
+++ src/main.ml
@@ -4,47 +4,39 @@ type color =
   | Black
   | White
 
-type model =
-  { moves : int
-  ; turn : color
-  }
+type model = {
+  moves : int;
+  turn : color;
+}
 
-let model =
-    { moves = 1
-    ; turn = White
-    }
+let model = { moves = 1; turn = White }
 
-type msg =
-  | Move
+type msg = Move
 
 let update model = function
-| Move ->
-  let turn =
-    begin match model.turn with
+  | Move ->
+      let turn =
+        match model.turn with
         | Black -> White
         | White -> Black
-      end in
+      in
 
-let moves = model.moves + 1 in { turn; moves }
+      let moves = model.moves + 1 in
+      { turn; moves }
 
 let view model =
   let open Html in
   div []
-    [ p [] [ Printf.sprintf "Move %d.  It is %s's move."
-                model.moves
-                (match model.turn with | Black -> "Black"
-                                      | White -> "White")
-              |> text
-            ]
-    ; p [] [ button
-                [ onClick Move ]
-                [ text "Make a move!" ]
-            ]
+    [
+      p []
+        [
+          Printf.sprintf "Move %d.  It is %s's move." model.moves
+            ( match model.turn with
+            | Black -> "Black"
+            | White -> "White" )
+          |> text;
+        ];
+      p [] [ button [ onClick Move ] [ text "Make a move!" ] ];
     ]
 
-let main =
-  App.beginnerProgram
-    { model
-    ; update
-    ; view
-    } 
+let main = App.beginnerProgram { model; update; view }
