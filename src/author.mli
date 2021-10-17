7d8a8ecb83a389c26cdc4bdc70570a1fdba2c291
Kate Meuse <katemeuse@gmail.com>
2021-10-17 11:36:12 -0400

    add author files

---
+++ src/author.mli
@@ -0,0 +1,32 @@
+(************************************************************
+   Copyright (C) 2021 Cornell University.
+   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
+   You may not redistribute this assignment, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(** CS 3110 Fall 2021 Final Project
+
+    @author Peter Cao (ppc49), Bryan Lu (byl29), Noah Braun (ncb48), Kate Meuse (kmm522) *)
+
+(* TODO: complete the academic integrity statement below, then delete
+   this TODO comment. *)
+
+(************************************************************
+
+   Academic Integrity Statement
+
+   We, the people named in the author comment above, have fully reviewed the
+   course academic integrity policies.  I have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If I did engage in such collaboration, here is the
+   list of other students with whom I collaborated, and a brief summary of that
+   collaboration:
+
+ ************************************************************)
+
+ val hours_worked : int
+ (** [hours_worked] is the number of hours you worked on this assignment. *)
+ 
