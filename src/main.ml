8d18d3a1692115c2796bc06de5c9778415f69aaf
Kate Meuse <katemeuse@gmail.com>
2021-10-28 15:23:18 -0400

    create basic link site nav, no home page, navtest

--- src/main.ml
+++ src/main.ml
@@ -1,22 +1,42 @@
 open Tea
 module Metapuzzle = Puzzlepage.M (Meta.M)
+module Navtest = Puzzlepage.M (Navtest.N)
 
-type model = { meta : Metapuzzle.model }
+type model = {
+  meta : Metapuzzle.model;
+  navtest : Navtest.model;
+  page : string;
+}
 (** [model] is a type representing a model of the entire site containing
     a single [puzzle] so far *)
 
 (** [init] is the initial state of the webpage *)
-let init () = ({ meta = fst (Metapuzzle.init "answer") }, Cmd.none)
+let init () url =
+  ( {
+      meta = fst (Metapuzzle.init "answer");
+      navtest = fst (Navtest.init "answer");
+      page = "#home";
+    },
+    Cmd.none )
 
 (** [msg] is the type containing different types of event handlers *)
-type msg = Puzzlepage_msg of Metapuzzle.msg [@@bs.deriving { accessors }]
+type msg =
+  | Metapuzzlepage_msg of Metapuzzle.msg
+  | Navtest_msg of Navtest.msg
+  | UrlChange of Web.Location.location
+[@@bs.deriving { accessors }]
 
 (** [update model] is the update loop that is called whenever an event
     is happened in the model *)
 let update model = function
-  | Puzzlepage_msg msg ->
+  | Metapuzzlepage_msg msg ->
       let meta, cmd = Metapuzzle.update model.meta msg in
-      ({ meta }, Cmd.map puzzlepage_msg cmd)
+      ({ model with meta }, Cmd.map metapuzzlepage_msg cmd)
+  | Navtest_msg msg ->
+      let navtest, cmd = Navtest.update model.navtest msg in
+      ({ model with navtest }, Cmd.map navtest_msg cmd)
+  | UrlChange loc ->
+      ({ model with page = loc.Web.Location.hash }, Cmd.none)
 
 (** [view model] renders the [model] into HTML, which will become a
     website *)
@@ -26,11 +46,25 @@ let view model =
     [ classList [ ("center-text", true) ] ]
     [
       h1 [] [ Printf.sprintf "Rat Hunt" |> text ];
-      p [] [ Metapuzzle.view model.meta |> map puzzlepage_msg ];
-      a [ href "http://localhost:5000" ] [ text "Hi" ];
+      p []
+        [
+          ( match model.page with
+          | "#meta" ->
+              Metapuzzle.view model.meta |> map metapuzzlepage_msg
+          | "#navtest" -> Navtest.view model.navtest |> map navtest_msg
+          | _ -> Printf.sprintf "Page Not Found" |> text );
+        ];
+      p [] [ a [ href ("#" ^ "meta") ] [ text "metapuzzle" ] ];
+      p [] [ a [ href ("#" ^ "navtest") ] [ text "navigation test" ] ];
     ]
 
 (** [main] starts the web app *)
 let main =
-  App.standardProgram
-    { init; update; view; subscriptions = (fun _ -> Sub.none) }
+  Navigation.navigationProgram urlChange
+    {
+      init;
+      update;
+      view;
+      subscriptions = (fun _ -> Sub.none);
+      shutdown = (fun _ -> Cmd.none);
+    }
