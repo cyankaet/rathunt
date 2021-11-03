da5af98b36d0f587d7eff125a25495091eb85c63
Kate Meuse <katemeuse@gmail.com>
2021-11-03 19:38:12 -0400

    remove word from test file

--- test/test.ml
+++ test/test.ml
@@ -3,6 +3,6 @@ open Puzzlepage
 open Meta
 open Crossword
 
-let x = print_endline "sugma"
-let first_tests = [ ("dummy test" >:: fun _ -> assert_equal 1 (4 - 3)) ]
+let x = print_endline "hello world"
 
+let first_tests = [ ("dummy test" >:: fun _ -> assert_equal 1 (4 - 3)) ]
