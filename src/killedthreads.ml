865ae255f110a51f93dc89e5e2e1a712a42bb55f
Peter Cao <ppc49@cornell.edu>
2021-12-11 02:06:55 -0500

    make navigation for killed threads

--- src/killedthreads.ml
+++ src/killedthreads.ml
@@ -1,15 +1,41 @@
 open Tea
 
 module M : Puzzle.S = struct
-  type t = unit
+  type t = {
+    solved : int list;
+    selected : int;
+    answers : string list;
+    box_text : string;
+  }
 
   type model = t
 
-  type msg = unit
+  type msg = Select of int | Solve of int | UpdateText of string
 
-  let init () = ((), Cmd.none)
+  let zero_to_five = [ 0; 1; 2; 3; 4; 5 ]
 
-  let update model msg = ((), Cmd.none)
+  let string_clean str =
+    Js.String.(toUpperCase str |> trim |> replaceByRe [%bs.re "/[^A-Za]/g"] "")
+
+  let answers =
+    "resources/killed_answers.txt" |> Node.Fs.readFileAsUtf8Sync
+    |> String.split_on_char '\n'
+
+  let init () =
+    ({ solved = []; selected = -1; answers; box_text = "" }, Cmd.none)
+
+  let update model = function
+    | Solve cell ->
+        if List.mem cell model.solved then (model, Cmd.none)
+        else ({ model with solved = cell :: model.solved }, Cmd.none)
+    | Select cell -> ({ model with selected = cell; box_text = "" }, Cmd.none)
+    | UpdateText str -> ({ model with box_text = str }, Cmd.none)
+    | _ -> (model, Cmd.none)
+
+  let rand_list = Rng.shuffle [ 1; 3; 5; 7; 9 ]
+
+  let rand_string =
+    List.fold_left (fun x y -> x ^ " " ^ string_of_int y) "" rand_list
 
   let view model =
     let open Html in
@@ -27,10 +53,71 @@ module M : Puzzle.S = struct
         p []
           [
             Printf.sprintf
-              "Oh hi [TEAM NAME], you're in charge of investigating the crime \
-               scenes and identifying any information about our killer. We'll \
-               take it from here."
+              {|"Oh hi there, you're in charge of investigating the crime scenes
+               and identifying any information about our killer. We'll take it
+               from there."|}
             |> text;
           ];
+        p []
+          [
+            Printf.sprintf "%s"
+              (List.fold_left
+                 (fun x y -> x ^ " " ^ string_of_int y)
+                 "" rand_list)
+            |> text;
+          ];
+        ( match model.selected with
+        | -1 ->
+            div []
+              [
+                div []
+                  [
+                    (let rec make_buttons = function
+                       | [] -> p [] []
+                       | curr :: rest ->
+                           div []
+                             [
+                               button
+                                 [
+                                   onClick (Solve curr);
+                                   classList [ ("submit", true) ];
+                                 ]
+                                 [
+                                   text
+                                     ( "Crime Scene "
+                                     ^ string_of_int (curr + 1)
+                                     ^ ": "
+                                     ^
+                                     if List.mem curr model.solved then
+                                       string_clean (List.nth answers curr)
+                                     else "????" );
+                                 ];
+                               make_buttons rest;
+                             ]
+                     in
+                     make_buttons zero_to_five);
+                  ];
+                ( if List.length model.solved = 6 then
+                  div []
+                    [
+                      p []
+                        [
+                          text
+                            {|"Hey, that was some good work there with the crime scenes. Oh, you want the autopsy files? Here you go."|};
+                        ];
+                      button
+                        [ onClick (Select 6); classList [ ("submit", true) ] ]
+                        [ text "View autopsy files" ];
+                    ]
+                else p [] [] );
+              ]
+        | _ ->
+            div []
+              [
+                p [] [ text ("Puzzle " ^ string_of_int model.selected) ];
+                button
+                  [ onClick (Select (-1)); classList [ ("submit", true) ] ]
+                  [ text "Go back" ];
+              ] );
       ]
 end
