2cdd135486f1a17614152ba9f797e51922d69569
Peter Cao <ppc49@cornell.edu>
2021-12-11 21:38:24 -0500

    make subpuzzles 2-5

--- src/killedthreads.ml
+++ src/killedthreads.ml
@@ -27,27 +27,64 @@ module M : Puzzle.S = struct
   let update model = function
     | Solve cell ->
         if List.mem cell model.solved then (model, Cmd.none)
-        else ({ model with solved = cell :: model.solved }, Cmd.none)
+        else if
+          string_clean model.box_text = string_clean (List.nth answers cell)
+        then
+          ({ model with solved = cell :: model.solved; box_text = "" }, Cmd.none)
+        else ({ model with box_text = "" }, Cmd.none)
     | Select cell -> ({ model with selected = cell; box_text = "" }, Cmd.none)
     | UpdateText str -> ({ model with box_text = str }, Cmd.none)
     | _ -> (model, Cmd.none)
 
-  let rand_list = Rng.shuffle [ 1; 3; 5; 7; 9 ]
+  let load_puzzle_content puzzle =
+    let lines =
+      match puzzle with
+      | 2 ->
+          "resources/crime_2.txt" |> Node.Fs.readFileAsUtf8Sync
+          |> String.split_on_char '\n'
+      | 3 ->
+          "resources/crime_3.txt" |> Node.Fs.readFileAsUtf8Sync
+          |> String.split_on_char '\n'
+      | 4 ->
+          "resources/crime_4.txt" |> Node.Fs.readFileAsUtf8Sync
+          |> String.split_on_char '\n'
+      | 5 ->
+          "resources/crime_5.txt" |> Node.Fs.readFileAsUtf8Sync
+          |> String.split_on_char '\n'
+      | _ -> failwith "impossible"
+    in
+    let open Html in
+    div []
+      [
+        p [] [ text (List.hd lines) ];
+        ol []
+          [
+            (let rec make_clues = function
+               | [] -> p [] []
+               | clue :: rest -> div [] [ li [] [ text clue ]; make_clues rest ]
+             in
+             make_clues (List.tl lines));
+          ];
+      ]
 
-  let rand_string =
-    List.fold_left (fun x y -> x ^ " " ^ string_of_int y) "" rand_list
+  let autopsy_files =
+    "resources/autopsy.txt" |> Node.Fs.readFileAsUtf8Sync
+    |> String.split_on_char '\n'
 
   let view model =
     let open Html in
     div []
       [
-        p []
+        i []
           [
-            Printf.sprintf
-              "There's ultimately nothing suspicious about a few new transfer \
-               students being murdered, right? Okay, they were a little \
-               talented."
-            |> text;
+            p []
+              [
+                Printf.sprintf
+                  "There's ultimately nothing suspicious about a few new \
+                   transfer students being murdered, right? Okay, they were a \
+                   little talented."
+                |> text;
+              ];
           ];
         p [] [];
         p []
@@ -58,16 +95,69 @@ module M : Puzzle.S = struct
                from there."|}
             |> text;
           ];
-        p []
-          [
-            Printf.sprintf "%s"
-              (List.fold_left
-                 (fun x y -> x ^ " " ^ string_of_int y)
-                 "" rand_list)
-            |> text;
-          ];
         ( match model.selected with
-        | -1 ->
+        | 6 ->
+            div []
+              [
+                h3 [] [ text "Autopsy Files" ];
+                p []
+                  [
+                    ol []
+                      [
+                        (let rec display_files = function
+                           | [] -> p [] []
+                           | file :: rest ->
+                               div []
+                                 [ li [] [ text file ]; display_files rest ]
+                         in
+                         display_files autopsy_files);
+                      ];
+                  ];
+                button
+                  [ onClick (Select (-1)); classList [ ("submit", true) ] ]
+                  [ text "Go back" ];
+              ]
+        | 0 | 1 | 2 | 3 | 4 | 5 ->
+            div []
+              [
+                h3 []
+                  [ text ("Crime Scene " ^ string_of_int (model.selected + 1)) ];
+                ( if List.mem model.selected model.solved then
+                  h5 [] [ text "Solved!" ]
+                else
+                  div []
+                    [
+                      p []
+                        [
+                          input'
+                            [
+                              type' "text";
+                              id "answer-bar";
+                              value model.box_text;
+                              onInput (fun s -> UpdateText s);
+                              placeholder "Enter Answer Here";
+                            ]
+                            [];
+                        ];
+                      p []
+                        [
+                          button
+                            [
+                              onClick (Solve model.selected);
+                              classList [ ("submit", true) ];
+                            ]
+                            [ text "Submit" ];
+                        ];
+                    ] );
+                ( if model.selected >= 1 && model.selected <= 4 then
+                  load_puzzle_content (model.selected + 1)
+                else if model.selected = 0 then p [] []
+                else p [] [] );
+                button
+                  [ onClick (Select (-1)); classList [ ("submit", true) ] ]
+                  [ text "Go back" ];
+              ]
+        | -1 | _ ->
             div []
               [
                 div []
@@ -79,7 +169,7 @@ module M : Puzzle.S = struct
                              [
                                button
                                  [
-                                   onClick (Solve curr);
+                                   onClick (Select curr);
                                    classList [ ("submit", true) ];
                                  ]
                                  [
@@ -110,14 +200,6 @@ module M : Puzzle.S = struct
                         [ text "View autopsy files" ];
                     ]
                 else p [] [] );
-              ]
-        | _ ->
-            div []
-              [
-                p [] [ text ("Puzzle " ^ string_of_int model.selected) ];
-                button
-                  [ onClick (Select (-1)); classList [ ("submit", true) ] ]
-                  [ text "Go back" ];
               ] );
       ]
 end
