c283e07a34bb3b6a392ce0b8293c6e69f50ade5c
Bryan Lu <byl29@cornell.edu>
2021-12-13 14:13:39 -0500

    about image fuckery

--- src/about.ml
+++ src/about.ml
@@ -4,11 +4,8 @@ type msg
 
 let view () =
   let open Html in
-  div
-    [ classList [] ]
+  div [ classList [] ]
     [
-      div
-        [ classList [] ]
-        [ node "frend" [ classList [ ("rat-img", true) ] ] [] ];
+      img ?unique:(Some "frend") [ classList [ ("rat-img", true) ] ] [];
       div [ classList [] ] [ h2 [] [ Printf.sprintf "About" |> text ] ];
     ]
