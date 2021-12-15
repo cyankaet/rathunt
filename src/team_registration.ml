d31180ee6e7f495e5a293911083373a923bacad7
Kate Meuse <katemeuse@gmail.com>
2021-12-15 18:31:46 -0500

    enlarge subtitle, add broken post request to backend

--- src/team_registration.ml
+++ src/team_registration.ml
@@ -1,51 +1,64 @@
 open Tea
 open Json.Decoder
 include Http_utils
+open Tea.Http
 
-type msg = CreateTeam | PostResponse of (string, string Http.error) Result.t
+type msg =
+  | CreateTeam
+  | PostResponse of (string, string Http.error) Result.t
 [@@bs.deriving { accessors }]
 
-type team = { name : string; solves : int; guesses : int; hints : int }
+type team = {
+  name : string;
+  solves : int;
+  password : string;
+}
 
 type model = string transfer
 
-let make_form_data name solves guesses hints =
+let make_form_data name solves password =
   [
     ("name", name);
     ("solves", string_of_int solves);
-    ("guesses", string_of_int guesses);
-    ("hints", string_of_int hints);
+    ("password", password);
   ]
 
 let init = Idle
 
-let default = make_form_data "Galactic Trendsetters" 10 2 11
+let default = make_form_data "Galactic Trendsetters" 3 "trendy"
 
-let url = "http://ptsv2.com/t/bar/post"
+let url = "http://rathunt-backend.herokuapp.com/team/new/"
 
 let update model = function
   | CreateTeam -> (
       match model with
-      | Loading | Received _ ->
+      | Loading
+      | Received _ ->
           print_endline "Loading";
           (model, Cmd.none)
-      | Idle | Failed ->
+      | Idle
+      | Failed ->
           print_endline "Hi";
-          (Loading, Http_utils.make_post_request url [] default postResponse) )
+          ( Loading,
+            Http_utils.make_post_request url
+              [ Header ("Access-Control-Allow-Origin", "*") ]
+              default postResponse ) )
   | PostResponse (Error e) ->
       Js.log (Http.string_of_error e);
       (Failed, Cmd.none)
   | PostResponse (Ok response) ->
       let response_decoder = field "response" string in
       let final =
-        (decodeString response_decoder) response |> Http_utils.result_to_string
+        (decodeString response_decoder) response
+        |> Http_utils.result_to_string
       in
       (Received final, Cmd.none)
 
 let view model =
   let open Html in
   match model with
-  | Idle -> p [] [ button [ onClick CreateTeam ] [ text "Make a team" ] ]
+  | Idle ->
+      p [] [ button [ onClick CreateTeam ] [ text "Make a team" ] ]
   | Loading -> p [] [ text "loading..." ]
   | Received response ->
       div [] [ p [] [ text "Team Created!" ]; p [] [ text response ] ]
